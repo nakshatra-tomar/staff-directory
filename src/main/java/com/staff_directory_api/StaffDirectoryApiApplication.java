ca192ae3960e749f877a131ed068a650f3eaf375
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-07-02 23:42:02 +0530

    init

---
+++ src/main/java/com/staff_directory_api/StaffDirectoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.staff_directory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffDirectoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffDirectoryApiApplication.class, args);
+	}
+
+}
