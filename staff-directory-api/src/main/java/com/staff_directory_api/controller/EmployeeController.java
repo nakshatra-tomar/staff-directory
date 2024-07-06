8bbd849f56f34e5f1b4fc2c23eefd00b9a302029
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-07-06 22:39:28 +0530

    add save employee func

--- staff-directory-api/src/main/java/com/staff_directory_api/controller/EmployeeController.java
+++ staff-directory-api/src/main/java/com/staff_directory_api/controller/EmployeeController.java
@@ -2,11 +2,9 @@ package com.staff_directory_api.controller;
 
 import com.staff_directory_api.model.Employee;
 import com.staff_directory_api.services.EmployeeService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
--- staff-directory-ui/src/components/AddEmployee.js
+++ staff-directory-ui/src/components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import EmployeeService from "../services/EmployeeService";
 
 const AddEmployee = () => {
   const [employee, setEmployee] = useState({
@@ -8,7 +9,23 @@ const AddEmployee = () => {
     emailId: "",
   });
 
-  useState();
+  const handleChange = (e) => {
+    //e is event object
+    const value = e.target.value; //55:30
+    setEmployee({ ...employee, [e.target.name]: value });
+  };
+
+  const saveEmployee = (e) => {
+    e.preventDefault();
+    EmployeeService.saveEmployee(employee)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="flex max-w-2xl mx-auto shadow-md border-b mt-10 bg-gray-100 rounded-lg">
       <div className="px-8 py-8 w-full">
@@ -22,7 +39,7 @@ const AddEmployee = () => {
           <input
             type="text"
             name="firstName"
-            value={employee.firstName} //first name input field
+            value={employee.firstName} // value tied to employee object //first name input field
             onChange={(e) => handleChange(e)}
             className="h-10 w-full border mt-2 px-2 py-2 rounded"
             placeholder="Enter employee's first name"
@@ -55,7 +72,10 @@ const AddEmployee = () => {
           />
         </div>
         <div className="flex items-center justify-between h-14 w-full my-4 space-x-4 pt-4">
-          <button className="rounded text-white font-semibold bg-green-500 hover:bg-green-700 py-2 px-4">
+          <button
+            onClick={saveEmployee}
+            className="rounded text-white font-semibold bg-green-500 hover:bg-green-700 py-2 px-4"
+          >
             Save
           </button>
           <button className="rounded text-white font-semibold bg-red-500 hover:bg-red-700 py-2 px-4">
